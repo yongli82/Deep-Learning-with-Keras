bcc6d41e8cc8d6b37b0494a063a81078436bda06
yangyongli <yangyongli@outlook.com>
2018-11-03 22:07:32 +0800

    inst comment

--- Appendix/legacy.py
+++ Appendix/legacy.py
@@ -1,18 +1,65 @@
+
 """
 Utility functions to avoid warnings while testing both Keras 1 and 2.
+
+legacy  ['legəsɪ] n. 遗赠，遗产
 """
 import keras
 
 keras_2 = int(keras.__version__.split(".")[0]) > 1  # Keras > 1
 
+"""
+fit_generator： 
 
+Fits the model on data yielded batch-by-batch by a Python generator.
+
+epoch  ['ɛpək] n. [地质] 世；新纪元；新时代；时间上的一点
+"""
 def fit_generator(model, generator, epochs, steps_per_epoch):
+    """
+
+    :param model:
+    :param generator:
+    :param epochs:
+    :param steps_per_epoch:
+    :return:
+
+    Arguments:
+        generator: A generator or an instance of `Sequence`
+          (`keras.utils.Sequence`)
+            object in order to avoid duplicate data
+            when using multiprocessing.
+            The output of the generator must be either
+            - a tuple `(inputs, targets)`
+            - a tuple `(inputs, targets, sample_weights)`.
+            This tuple (a single output of the generator) makes a single batch.
+            Therefore, all arrays in this tuple must have the same length (equal
+            to the size of this batch). Different batches may have different
+              sizes.
+            For example, the last batch of the epoch is commonly smaller than
+              the
+            others, if the size of the dataset is not divisible by the batch
+              size.
+            The generator is expected to loop over its data
+            indefinitely. An epoch finishes when `steps_per_epoch`
+            batches have been seen by the model.
+        steps_per_epoch: Total number of steps (batches of samples)
+            to yield from `generator` before declaring one epoch
+            finished and starting the next epoch. It should typically
+            be equal to the number of samples of your dataset
+            divided by the batch size.
+            Optional for `Sequence`: if unspecified, will use
+            the `len(generator)` as a number of steps.
+        epochs: Integer, total number of iterations on the data.
+    """
     if keras_2:
         model.fit_generator(generator, epochs=epochs, steps_per_epoch=steps_per_epoch)
     else:
         model.fit_generator(generator, nb_epoch=epochs, samples_per_epoch=steps_per_epoch)
 
-
+"""
+fit
+"""
 def fit(model, x, y, nb_epoch=10, *args, **kwargs):
     if keras_2:
         return model.fit(x, y, *args, epochs=nb_epoch, **kwargs)
--- Chapter01/keras_MINST_V1.py
+++ Chapter01/keras_MINST_V1.py
@@ -6,37 +6,48 @@ from keras.layers.core import Dense, Activation
 from keras.optimizers import SGD
 from keras.utils import np_utils
 
+# 固定随机数种子，使每次产生的随机数保持一致
 np.random.seed(1671)  # for reproducibility
 
 # network and training
+# 世代
 NB_EPOCH = 200
+# 批量
 BATCH_SIZE = 128
+# 显示详细日志  adj. 冗长的；啰嗦的
 VERBOSE = 1
+# 输出分类数量 这里等于 10个数字
 NB_CLASSES = 10   # number of outputs = number of digits
+# 优化器 SGD
 OPTIMIZER = SGD() # SGD optimizer, explained later in this chapter
+#
 N_HIDDEN = 128
+# 训练数据分割比例
 VALIDATION_SPLIT=0.2 # how much TRAIN is reserved for VALIDATION
 
 # data: shuffled and split between train and test sets
-#
+# 加载已经预置的mnist训练和测试数据集
+# 其中X是数字图形的灰度二维矩阵集合，Y是对应的数字 0~9
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 #X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
+# 本来每个图像是 28 x 28的二维矩阵，这里把它转换给一维序列
 RESHAPED = 784
-#
 X_train = X_train.reshape(60000, RESHAPED)
 X_test = X_test.reshape(10000, RESHAPED)
+# 转换成浮点表示
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
 # normalize 
-#
+# 把特征数据从0~255的灰度值归一到 0 ~ 1.0的浮点数
 X_train /= 255
 X_test /= 255
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
+# 把输出值从数字转换成分类数据，避免0~9的数值大小的相关影响
 Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
 Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
 
